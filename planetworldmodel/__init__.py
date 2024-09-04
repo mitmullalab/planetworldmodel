94481904d61bae5df40651b344e20b9a6d094042
Peter G. Chang <peter0414@gmail.com>
2024-09-04 14:19:31 -0400

    Move init to outer init

---
+++ planetworldmodel/__init__.py
@@ -0,0 +1,3 @@
+from .src.two_body_problem import TwoBodyProblem, generate_trajectories
+
+__all__ = ["TwoBodyProblem", "generate_trajectories"]
--- planetworldmodel/src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .two_body_problem import TwoBodyProblem, generate_trajectories
-
-__all__ = ["TwoBodyProblem", "generate_trajectories"]
