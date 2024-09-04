2ef9153a5354f01f68d483efd52a7617f34d1084
Peter G. Chang <peter0414@gmail.com>
2024-09-04 17:10:18 -0400

    Implement random two-body problem

--- planetworldmodel/__init__.py
+++ planetworldmodel/__init__.py
@@ -1,3 +1,7 @@
-from .src.two_body_problem import TwoBodyProblem, generate_trajectories
+from .src.two_body_problem import (
+    TwoBodyProblem,
+    generate_trajectories,
+    random_two_body_problem,
+)
 
-__all__ = ["TwoBodyProblem", "generate_trajectories"]
+__all__ = ["TwoBodyProblem", "generate_trajectories", "random_two_body_problem"]
