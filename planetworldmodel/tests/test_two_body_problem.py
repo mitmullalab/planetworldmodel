ce0a0e1867ef724e5b7d1e90e69da974128e4d47
Peter G. Chang <peter0414@gmail.com>
2024-09-05 16:16:30 -0400

    Revert to numpy methods

--- planetworldmodel/tests/test_two_body_problem.py
+++ planetworldmodel/tests/test_two_body_problem.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize("target_eccentricity", [0.2, 0.5, 0.9, 1.0, 1.1, 10.0])
-def test_random_two_body_problem(target_eccentricity, tol=1e-3):
-    two_body_problem = random_two_body_problem(target_eccentricity)
+def test_random_two_body_problem(target_eccentricity, tol=1e-3, seed=0):
+    two_body_problem = random_two_body_problem(target_eccentricity, seed)
     _, _, eccentricity = generate_trajectories(two_body_problem)
     assert abs(eccentricity - target_eccentricity) <= tol
