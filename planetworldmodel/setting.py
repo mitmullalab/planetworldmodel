1ff8e9e3f3632ec406f724024c4f51f78f3e6f6c
Peter G. Chang <peter0414@gmail.com>
2024-09-19 08:20:08 -0400

    Update training script and change to `RMSE` loss (#16)
    * Add gen data dir to setting
    
    * Adjust configuration
    
    * Increase number of data points generated
    
    * Move data generated to generated data directory
    
    * Change loss function from MSE to RMSE
    
    * Add demo notebook
    
    * Clean up notebook

--- planetworldmodel/setting.py
+++ planetworldmodel/setting.py
@@ -4,3 +4,4 @@ BASE_DIR = Path(__file__).resolve().parent
 CKPT_DIR = BASE_DIR / "checkpoints"
 CONFIG_DIR = BASE_DIR / "configs"
 DATA_DIR = BASE_DIR / "data"
+GEN_DATA_DIR = DATA_DIR / "generated_data"
