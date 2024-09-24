6ad0fc477461afc872bafcd510718302c6e0196e
Peter G. Chang <peter0414@gmail.com>
2024-09-24 12:05:35 -0400

    Add function to init

--- planetworldmodel/__init__.py
+++ planetworldmodel/__init__.py
@@ -1,6 +1,7 @@
 from .src.two_body_problem import (
     TwoBodyProblem,
     generate_trajectories,
+    generate_trajectory_with_heavier_fixed,
     random_two_body_problem,
 )
 from .src.utils import TransformerConfig, load_config, setup_wandb
@@ -12,6 +13,7 @@ __all__ = [
     "TwoBodyProblem",
     "load_config",
     "generate_trajectories",
+    "generate_trajectory_with_heavier_fixed",
     "random_two_body_problem",
     "setup_wandb",
 ]
